8d5ec8388e5eb1b24c13068f7c618372d232bdf8
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-20 23:43:27 +0530

    Update officialx.js

--- officialx.js
+++ officialx.js
@@ -4,27 +4,23 @@ async function main() {
   const client = new Client("your bearer token here");
 
   const response = await client.tweets.tweetsRecentSearch({
-    "max_results": 10,
-    "sort_order": "relevancy",
-    "tweet.fields": [
-        "geo"
-    ],
-    "expansions": [
-        "geo.place_id"
-    ],
+    max_results: 10,
+    sort_order: "relevancy",
+    "tweet.fields": ["geo"],
+    expansions: ["geo.place_id"],
     "place.fields": [
-        "contained_within",
-        "country",
-        "country_code",
-        "full_name",
-        "geo",
-        "id",
-        "name",
-        "place_type"
-    ]
+      "contained_within",
+      "country",
+      "country_code",
+      "full_name",
+      "geo",
+      "id",
+      "name",
+      "place_type",
+    ],
   });
-  
+
   console.log("response", JSON.stringify(response, null, 2));
 }
-  
+
 main();
