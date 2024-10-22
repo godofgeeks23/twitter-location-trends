096b239f7b6ee1c27268bee5fef941899c791494
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-22 23:39:39 +0530

    Create official_x.js

---
+++ official_x.js
@@ -0,0 +1,27 @@
+// official twitter-api-sdk example
+import { Client } from "twitter-api-sdk";
+
+async function main() {
+  const client = new Client("your bearer token here");
+
+  const response = await client.tweets.tweetsRecentSearch({
+    max_results: 5,
+    sort_order: "relevancy",
+    "tweet.fields": ["geo"],
+    expansions: ["geo.place_id"],
+    "place.fields": [
+      "contained_within",
+      "country",
+      "country_code",
+      "full_name",
+      "geo",
+      "id",
+      "name",
+      "place_type",
+    ],
+  });
+
+  console.log("response", JSON.stringify(response, null, 2));
+}
+
+main();
